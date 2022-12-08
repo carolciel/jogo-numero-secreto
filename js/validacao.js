930c47d1588e860b19ed71d591f226d7be734d77
Carolina Gomes <100874985+carolciel@users.noreply.github.com>
2022-12-08 16:13:56 -0300

    Primeira publicação do jogo

---
+++ js/validacao.js
@@ -0,0 +1,46 @@
+function verificaValorValido(chute) {
+    const numero = +chute;
+
+    if(chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>';
+        return 
+    }
+
+    if(numeroForMaiorOuMenorQuePermitido(numero)) {
+        elementoChute.innerHTML += `
+            <div>Valor inválido, fale um número 
+            entre ${menorValor} e ${maiorValor}</div>
+        `;
+        return
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>
+        `
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroForMaiorOuMenorQuePermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
